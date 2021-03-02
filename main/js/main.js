ea421569786ea982f16c71e8139b74b3069aad99
colehennnig <77035076+colehennnig@users.noreply.github.com>
2021-03-01 21:42:41 -0800

    main

--- main/js/main.js
+++ main/js/main.js
@@ -1,1 +1,82 @@
+var phobias = [['Spiders', 'Aracnaphobia is the fear of spiders', 'Aracno', 'creature', 'Spiders are one of the most common fears. If you have arachnophobia, you may be too fearful to kill or trap spiders yourself. When encountering a spider you may flee or even become frozen in place. A popular theory is that humans evolved to have arachnophobia as a survival technique to help us avoid potentially dangerous insects.'],
+              ['Snakes', 'Ophidiophobia is the fear of snakes', 'Ophis', 'creature', 'People with ophidiophobia experience anxiety and/or a physical reaction when being close to or thinking about snakes. This phobia can be caused by having or hearing about negative experiences. It can also be caused by their portrayal in media. A 2013 study also found that we are evolutionarily conditioned to fear snake-like forms.'],
+              ['Heights', 'Acrophobia is the fear of heights ', 'Acro', 'situational', 'Many people experience anxiety when looking down from high places. Acrophobia can extend to feelings of discomfort even when seeing a photo of a high place or thinking about something like crossing a bridge. Acrophobia can be caused by traumaitc experiences. Phobias can also develop without a clear cause, genetics and environmental factors may play a role.'],
+              ['Dogs', 'Cynophobia is the fear of dogs', 'Cyno', 'creature', 'Fear of dogs is usually caused by a negative experience, especially during childhood. Fear of snakes and spiders is more prevalent, but the average person is much more likely to encounter dogs on a regular basis. This makes the phobia much worse for those who experience it. Thunder and'],
+              ['Lightning', 'Astraphobia is the fear of thunder, lightning, and storms', 'Astro', 'environmental', 'Many people with astraphobia seek more shelter or protction from a storm. This may look like hiding under your bed, basement, bathroom, or closet. This phobia is very common in children and even animals too.'],
+              ['Injections', 'Trypanophobia is the far of injections or needles', 'Trypano', 'thing', 'Someone who experiences trypanophobia has an extreme fear of injections or needles. This phobia is common in children. It can be caused by general anxiety, negative experiences, sensitivity to pain, and can even be connected to the fear of being restrained.'],
+              ['Flying', 'Aviophobia', 'Avi', 'situational', 'People with aviophobia experience an intense fear of/disdain towards flying. People who suffer from this phobia often have physical reactions to the thought or experience of flying. This phobia may stem from related factors such as being in an enclosed space (claustrophobia), fear of crowds (agoraphobia), and/or heights (acrophobia).'],
+              ['Social Situations', 'Social Phobia / Anthrophobia ', 'Anthro', 'situational', 'People with Social phobia (also known as Social Anxiety Disorder) experience an irrational fear of being watched, judged, embarrassed, or humiliating themselves. It is believed that this disorder is caused by a combination of genetic and environmental factors. Social Phobia is considered to be one of the most common mental disorders. If you find that you suffer from this, there is a wide variety of therapies and medications used to treat Social Anxiety Disorder. Dirt or'],
+              ['Germs', 'Mysophobia', 'Myso', 'thing', 'Someone who experiences mysophobia has a fear of dirt, germs, and contamination. If you suffer from this phobia you may experience shaking, sweating, heart palpitations, or crying when exposed to dirt or bacteria. Mysophobia is thought to be related to obsessive-compulsive disorder. If you experience either, it is important to consult with a mental health professional.'],
+              ['Clowns', 'Coulrophobia', 'Coulro', 'creature', 'The fear of clowns is thought to be caused by negative personal expierces or the media portrayal of clowns in horror. Fear of clowns is prevalent in children as the exaggerated facial features and expressions create uncanny human-like figures. Confined'],
+              ['Spaces', 'Claustrophobia', 'Claustro', 'situational', 'If you suffer from claustrophobia you may experience anxiety, panic attacks, difficulty breathing, or feeling like the walls are closing in on you. Claustrophobia is one of the most prevalent fears and there are a wide variety of symptoms someone can expierince in response to being enclosed in a small place like a closet, elevator, crawlspace, or even crowds. Many theorize that this phobia is caused by bad childhood experiences or an evolutionary survival mechanism. The'],
+              ['Ocean', 'Thalassophobia', 'Thalasso', 'environmental', 'People who suffer from thalassophobia fear what could be lurking under the surface of vast, dark, and deep bodies of water. Common sypmtons include shortness of breath, racing heart, nausea, and a sense of imminent doom when in or near bodies of water. This is different from aquaphobia (the fear of water) and is likely caused by genetic, environmental facotrs, and/or personal experiences. Public'],
+              ['Speaking', 'Glossophobia', 'Glosso', 'situational', 'The fear of public speaking is very common, most of the population experiences some anxiety when it comes of public speaking. Glossophoia is related to social phobia and stage fright. In children, it may appear as desperately hoping to not get called on to answers a question. This phobia may drive you to avoid situations where you become the focus of attention. The'],
+              ['Dark', 'Nyctophobia', 'Nycto', 'environmental', 'Many people have felt a fear of the dark before in their life. Nyctophobia is not age specific fear. It is also known as scotophobia, achluophobia, and lygophobia. Fear of the dark may be evolutionary as many predators hunt at night. This fear may not be darkness itself but the unknown dangers hidden in it. People wih nyctophobia may expieircne nervousness in any darkened environment, needing to sleep with a nightlight, and physical discomfort when forced to spend time in the'],
+              ['Tornadoes and Hurricanes', 'Lilapsophobia', 'Lilapso', 'environmental', 'Lilapsophobia is considered the most intense form of astraphobia, which is the fear of thunder and lightning. It is the fear of tornados or hurricanes.'],
+              ['Crowds or Open Spaces', 'Agoraphobia', 'Agora', 'environmental', "When one is afriad of leaving their space and entering public places or crowds, they are considered to have agoraphobia. More specifically, it is a fear of being in a situation of place that is unescapable or out of one's control."],
+              ['Faliure', 'Atychiphobia', 'Atyches', 'situational', "Atychiphobia is the fear of failure and is often a roadblock to people's achievement because it prevents people from taking the first steps of trying."],
+              ['Imperfection', 'Atelophobia', 'Atelo', 'situational', 'Similar to perfectionism, Atelophobia belongs to a group of anxiety disorders. It can prevent people from encountering situations that could result in failure.'],
+              ['Being Alone', 'Autophobia', 'Autos', 'situational', 'People with Autophobia feel as if they need another person or people around to feel safe. This could stem from a fear of not being able to protect oneself or from a fear of being unloved.'],
+              ['Humanoid Figures', 'Automatonophobia', 'Automatos', 'creature', "Somebody that suffers from Automatonophobia can be afrain of human-like figures such as robots, wax figures, mannequins, statues, or dummies. This phobia can develop because of a traumatic experience, genetics, one's environment, or developmental experiences."],
+              ['Insects', 'Entomophobia', 'Entom', 'creature', 'Many are afflicted with the fear of insects and it is one of the most common animal phobias. This phobia usually develops after a traumatic experience with insects and can develop early or late in life.'], ['Blood', 'Hemophobia', 'Hemo', 'thing', "Hemophobia can prevent sufferers from participating in activities that involve blood such as doctor's appointments. The sight of blood causes a vasovagal response- which entails a drop in blood pressure and heart rate. This causes sufferers to feel dizzy."]];
 
+
+function create(element) {
+  var d = document.createElement("div");
+  d.setAttribute("id", element);
+  d.setAttribute("class", "main");
+  var head = document.createElement("div");
+  d.appendChild(head);
+  head.setAttribute("id", element + "header");
+  head.setAttribute("class", "head");
+  head.innerHTML = element;
+  document.getElementById("fears").appendChild(d);
+  d.style.top = Math.floor((Math.random() * 700) + 1) + "px";
+  d.style.left = Math.floor((Math.random() * 1100) + 200) + "px";
+  dragElement(d);
+}
+
+for (var i = 0; i < phobias.length; i++) {
+  create(phobias[i][0]);
+}
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
